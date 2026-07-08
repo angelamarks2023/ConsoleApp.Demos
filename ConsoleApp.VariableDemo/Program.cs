6fed1f1bd16309235f1e1389cdd0593c48d13435
angelamarks2023 <ama2651@wgu.edu>
2026-07-08 08:33:49 -0500

    Add project files.

---
+++ ConsoleApp.VariableDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Different datatypes
+/*
+    text - string
+    integers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Angela Marks";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // String concatenation
+Console.WriteLine($"They call me {name}"); // String interpolation
+Console.WriteLine("I was given the name {0}", name); // Formatted string
+
+int age = 54;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+Console.WriteLine("Am I retired? " + isRetired);
